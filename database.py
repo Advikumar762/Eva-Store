11e5f471a656cdf4953ecc299d527261029ad43c
Advik kumar <144226115+Advikumar762@users.noreply.github.com>
2023-12-18 05:38:23 +0530

    Update database.py

--- database.py
+++ database.py
@@ -65,3 +65,32 @@ class Database:
         return total_link_count
 
 db = Database(DATABASE_URL, BOT_USERNAME) 
+
+def add_user(bot: Client, cmd: Message):
+    if not await db.is_user_exist(cmd.from_user.id):
+        await db.add_user(cmd.from_user.id)
+        if LOG_CHANNEL is not None:
+            await bot.send_message(
+                chat_id=LOG_CHANNEL,
+                text=f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
+    )
+
+async def handle_user_status(bot, cmd):
+    chat_id = cmd.from_user.id
+    if not await db.is_user_exist(chat_id):
+        await db.add_user(chat_id)
+        await bot.send_message(
+            Config.LOG_CHANNEL,
+            f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
+        )
+
+    ban_status = await db.get_ban_status(chat_id)
+    if ban_status["is_banned"]:
+        if (
+                datetime.date.today() - datetime.date.fromisoformat(ban_status["banned_on"])
+        ).days > ban_status["ban_duration"]:
+            await db.remove_ban(chat_id)
+        else:
+            await cmd.reply_text("You are Banned to Use This Bot 🥺", quote=True)
+            return
+    await cmd.continue_propagation()
